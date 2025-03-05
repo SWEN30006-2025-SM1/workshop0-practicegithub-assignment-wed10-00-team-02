582ba13e3196ef7eb2167925d6703b98a1bbc903
Zijie Duan <zddua@student.unimelb.edu.au>
2025-03-05 11:38:26 +1100

    impliment -, *, /

--- app/src/main/java/example/Calculator.java
+++ app/src/main/java/example/Calculator.java
@@ -3,11 +3,10 @@ package example;
 public class Calculator {
     public int add(int a, int b) {
         // TODO: Implement this method
+        // Dummy code changes
+        // Changed addition function
         return (a + b);
     }
-
-
-
 
     public int minus(int a, int b) {
         // TODO: Implement this method
@@ -21,6 +20,6 @@ public class Calculator {
 
     public double divide(int a, int b) {
         // TODO: Implement this method
-        return 5;
+        return ((double) a / b);
     }
 }
